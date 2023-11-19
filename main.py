5d37437dd4f11a3ab3cc82ef9bc3a711358a4b9b
triglan <glanb4277@gmail.com>
2023-11-19 18:47:23 +0900

    drill13

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pico2d import open_canvas, delay, close_canvas
 import game_framework
 
-import pla`y_mode as start_mode
+import play_mode as start_mode
 
 open_canvas(1280, 1024)
 game_framework.run(start_mode)
